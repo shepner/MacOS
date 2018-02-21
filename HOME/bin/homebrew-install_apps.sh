1cc1a8078190223e8ebdca9f28a1c8ecd73cd6fc
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2018-02-20 19:47:44 -0600

    Create homebrew-install_apps.sh

---
+++ HOME/bin/homebrew-install_apps.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+brew update
+brew tap caskroom/cask
+
+#brew cask search virtual  # Example of how to search caskroom
+
+brew cask install virtualbox
+brew cask install virtualbox-extension-pack
+
+brew cask install evernote
+
+brew cask install path-finder
+
+brew cask install dropbox
+
+brew cask install firefox
+brew cask install google-chrome
+
+brew cask install istat-menus
+
+brew cask install keepingyouawake
+
+brew install vim
+
+brew cask install steam
+
+brew cask install vlc
+
+brew install r
+brew cask install rstudio
