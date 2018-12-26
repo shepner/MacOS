ff1fcfd35f662195ffec7ef09fd47b1554cc895d
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2018-12-25 20:01:29 -0600

    Create install_perlbrew.sh

---
+++ HOME/bin/install_perlbrew.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# https://perlbrew.pl/
+
+WORKDIR=$HOME/local
+PERLBREW_ROOT=$WORKDIR/perl5/perlbrew
+
+\curl -L https://install.perlbrew.pl | bash
+
+
+
+cat >> $HOME/.profile << EOF
+
+# perlbrew
+if [ -d $HOME/local/perl5/perlbrew ] ; then
+	export PERLBREW_ROOT=$HOME/local/perl5/perlbrew
+	source ${PERLBREW_ROOT}/etc/bashrc
+  perlbrew install perl-5.28.1
+	perlbrew switch perl-5.28.1
+fi
+
+EOF
