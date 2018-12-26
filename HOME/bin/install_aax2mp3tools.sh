53cfa7306edd7f2f93f4175a17c65cf8bdf2226b
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2018-12-25 18:07:35 -0600

    Update install_aax2mp3tools.sh

--- HOME/bin/install_aax2mp3tools.sh
+++ HOME/bin/install_aax2mp3tools.sh
@@ -15,11 +15,11 @@ pip3 install requests
 pip3 install selenium
 
 wget -O $DOWNLOADS/audible-activator.zip https://github.com/inAudible-NG/audible-activator/archive/master.zip
-unzip $DOWNLOADS/audible-activator.zip -d $WORKDIR/audible-activator -j
+unzip -jf $DOWNLOADS/audible-activator.zip -d $WORKDIR/audible-activator 
 ln -s $WORKDIR/audible-activator/audible-activator.py $HOME/bin/audible-activator.py
 
 wget -P $DOWNLOADS https://chromedriver.storage.googleapis.com/2.33/chromedriver_mac64.zip
-unzip $DOWNLOADS/chromedriver_mac64.zip -d $WORKDIR/audible-activator -j
+unzip -jf $DOWNLOADS/chromedriver_mac64.zip -d $WORKDIR/audible-activator
 
 ###
 
@@ -29,5 +29,5 @@ unzip $DOWNLOADS/chromedriver_mac64.zip -d $WORKDIR/audible-activator -j
 brew install ffmpeg
 
 wget -O $DOWNLOADS/AAXtoMP3.zip https://github.com/KrumpetPirate/AAXtoMP3/archive/master.zip
-unzip $DOWNLOADS/AAXtoMP3.zip -d $WORKDIR/AAXtoMP3 -j
+unzip -jf $DOWNLOADS/AAXtoMP3.zip -d $WORKDIR/AAXtoMP3
 ln -s $WORKDIR/AAXtoMP3/AAXtoMP3 $HOME/bin/AAXtoMP3
