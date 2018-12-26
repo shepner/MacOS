34983f630ef06a04a5c0deb366c626242a667b22
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2018-12-25 20:50:21 -0600

    Update install_perlbrew.sh

--- HOME/bin/install_perlbrew.sh
+++ HOME/bin/install_perlbrew.sh
@@ -12,10 +12,10 @@ cat >> $HOME/.profile << EOF
 
 # perlbrew
 if [ -d $HOME/local/perl5/perlbrew ] ; then
-	export PERLBREW_ROOT=$HOME/local/perl5/perlbrew
-	source ${PERLBREW_ROOT}/etc/bashrc
+  export PERLBREW_ROOT=$HOME/local/perl5/perlbrew
+  source ${PERLBREW_ROOT}/etc/bashrc
   perlbrew install perl-5.28.1
-	perlbrew switch perl-5.28.1
+  perlbrew switch perl-5.28.1
 fi
 
 EOF
