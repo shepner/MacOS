6c5423fba2012e54bf8c29f8d22f9e4a93c7db8d
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2018-12-27 16:04:32 -0600

    Update outbound_ssh_tunnel.sh

--- HOME/bin/outbound_ssh_tunnel.sh
+++ HOME/bin/outbound_ssh_tunnel.sh
@@ -5,8 +5,17 @@
 #
 # from <workstation1>, ssh to <central ssh server> on port 10000 and login as <user2>
 #   forward port 2222 from <central ssh server> to port 22 on <workstation1>
-#ssh -p 10000 -R 2222:localhost:22 <user2>@<central ssh server>
-ssh -nNT -p 10000 -R 2222:localhost:22 <user2>@<central ssh server>
+#   and DONT open a shell and run in the background
+#CMD="ssh -nNTf -p 10000 -R 2222:localhost:22 <user2>@<central ssh server>"
+CMD="ssh -nNTf -p 10000 -R 2222:localhost:22 <user2>@<central ssh server>"
+
+# run the command if needed
+if [ `ps axwww | grep "$CMD" | grep -c -v 'grep'` -eq 0 ] ; then
+  echo "starting ssh tunnel"
+  `$CMD`
+else
+  echo "ssh tunnel already running"
+fi
 
 # on <central ssh server> run the following:
 #cat >> /etc/ssh/sshd_config << EOF
@@ -18,3 +27,10 @@ ssh -nNT -p 10000 -R 2222:localhost:22 <user2>@<central ssh server>
 #   and login as <user1> on <workstation1>
 # DONT THIS RUN FROM HERE!
 #ssh -p 2222 <user1>@<central ssh server>
+
+# then do the following to enable key authentication
+#ssh-copy-id -i ~/.ssh/<ident> -p 2222 <user1>@<central ssh server>
+
+# if desired, use sshuttle to establish a 'VPN'
+# https://github.com/sshuttle/sshuttle
+#sshuttle -r <user1>@<central ssh server>:2222 <0.0.0.0/0>
