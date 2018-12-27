fcd4eb39cabe0edc5982a6d185d9382993231226
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2018-12-27 09:45:55 -0600

    Update update_homebrew_apps.sh

--- HOME/bin/update_homebrew_apps.sh
+++ HOME/bin/update_homebrew_apps.sh
@@ -1,43 +1,44 @@
 #!/bin/sh
+# Update everything in homebrew and install stuff if needed
+#
+# the search command will perform a case-insensitive search across all available repos
+# brew search <name>
+
 
 brew update    # update homebew itself
+brew tap caskroom/cask  # this is where the non-OpenSource stuff is stored
+
+###############################################
 
 brew list  # lets see whats already installed
+brew cask list  # lets see whats already installed
 echo "Press a key to continue" ; read
+
+###############################################
+
 brew outdated  # check to see what isnt needed anymore
 brew upgrade   # update the everything
 
-
-brew tap caskroom/cask  # this is where the non-OpenSource stuff is stored
-brew cask list  # lets see whats already installed
-echo "Press a key to continue" ; read
 brew cask outdated  # check to see what isnt needed anymore
 brew cask upgrade   # update the everything
 
-
 brew cleanup   # get rid of the old versions
-
-
-# the search command will perform a case-insensitive search across all available repos
-#brew search <name>
 
 ###############################################
 brew install wget
-brew install python3
-brew install python2
-
-brew install tintin
-
-
-###############################################
-brew cask install keepingyouawake
-brew cask install firefox
-#brew cask installthunderbird
-brew cask install google-chrome
 brew cask install macvim
+brew cask install keepingyouawake
 brew cask install grandperspective
-
 brew cask install istat-menus
+#brew cask install path-finder
+
+brew cask install firefox
+#brew cask install thunderbird
+brew cask install google-chrome
+
+brew cask install parallels
+#brew cask install virtualbox
+#brew cask install virtualbox-extension-pack
 
 brew cask install evernote
 brew cask install dropbox
@@ -51,15 +52,12 @@ brew cask install tinymediamanager
 #brew cask install mp3tag
 
 brew cask install steam
-
-brew cask install parallels
-#brew cask install virtualbox
-#brew cask install virtualbox-extension-pack
+brew install tintin
 
 brew cask install arduino
 brew cask install fritzing
-brew cask install balenaetcher
 #brew install r
 #brew cask install rstudio
-
-#brew cask install path-finder
+brew install python3
+brew install python2
+brew cask install balenaetcher
