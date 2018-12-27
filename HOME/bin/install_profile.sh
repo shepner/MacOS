ea55cd5f07edda47c1ad54676397d2072e875330
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2018-12-27 09:36:20 -0600

    Update install_profile.sh

--- HOME/bin/install_profile.sh
+++ HOME/bin/install_profile.sh
@@ -1,18 +1,12 @@
 #!/bin/sh
+# download and overwrite the profile
 
 WORKDIR=$HOME/tmp/MacOS
 mkdir -p $WORKDIR
 
-# This will prompt you to install the XCode command line tools
-xcode-select --install
-
 # Download/install the scripts
 git clone https://github.com/shepner/MacOS.git $WORKDIR
-cp -R $WORKDIR/HOME/ $HOME
-chmod -R 750 $HOME/bin
+cp -R $WORKDIR/HOME/.* $HOME
 
 # Clean up after ourselves
 rm -Rf $WORKDIR
-
-# Setup the database for locate
-sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.locate.plist
