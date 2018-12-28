93405f6478847810f40a97fa37abe2e5978624d1
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2018-12-28 10:57:22 -0600

    Update install_applications.sh

--- HOME/bin/install_applications.sh
+++ HOME/bin/install_applications.sh
@@ -25,24 +25,28 @@ brew cask install parallels
 #brew cask install libreoffice
 #brew cask install openoffice
 brew cask install evernote
+
 brew cask install dropbox
+
 brew cask install quicken
 brew cask install a-better-finder-rename
+brew cask install mcgimp
 
-#install_aax2mp3tools.sh
 brew cask install vlc
 brew cask install plex-media-player
+
 brew cask install tinymediamanager
 #brew cask install handbrake
 #brew cask install mp3tag
+#install_aax2mp3tools.sh
 
 brew cask install steam
 brew install tintin
 
+brew install python3
+brew install python2
 brew cask install arduino
 brew cask install fritzing
 #brew install r
 #brew cask install rstudio
-brew install python3
-brew install python2
 brew cask install balenaetcher
