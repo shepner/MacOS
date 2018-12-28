13056cbee0e5d59656b8549593d5642bc4b5966d
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2018-12-27 20:27:37 -0600

    Create install_applications.sh

---
+++ HOME/bin/install_applications.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+#install_perlbrew.sh
+#install_macports.sh
+install_homebrew.sh
+update_homebrew.sh
+
+
+###############################################
+brew install wget
+brew cask install macvim
+brew cask install keepingyouawake
+brew cask install grandperspective
+brew cask install istat-menus
+#brew cask install path-finder
+
+brew cask install firefox
+#brew cask install thunderbird
+brew cask install google-chrome
+
+brew cask install parallels
+#brew cask install virtualbox
+#brew cask install virtualbox-extension-pack
+
+brew cask install evernote
+brew cask install dropbox
+brew cask install quicken
+brew cask install a-better-finder-rename
+
+#install_aax2mp3tools.sh
+brew cask install vlc
+brew cask install plex-media-player
+brew cask install tinymediamanager
+#brew cask install handbrake
+#brew cask install mp3tag
+
+brew cask install steam
+brew install tintin
+
+brew cask install arduino
+brew cask install fritzing
+#brew install r
+#brew cask install rstudio
+brew install python3
+brew install python2
+brew cask install balenaetcher
