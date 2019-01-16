2158041275e8671116984b2f21ee864ff3fe3d55
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2019-01-16 09:45:03 -0600

    Update install_profile.sh

--- HOME/bin/install_profile.sh
+++ HOME/bin/install_profile.sh
@@ -6,7 +6,7 @@ mkdir -p $WORKDIR
 
 # Download/install the scripts
 git clone https://github.com/shepner/MacOS.git $WORKDIR
-cp -R $WORKDIR/HOME/.* $HOME
+cp $WORKDIR/HOME/.* $HOME
 
 # Clean up after ourselves
 rm -Rf $WORKDIR
