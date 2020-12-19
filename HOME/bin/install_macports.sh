14693ec3ca372e6e18f498e7011f84845be68437
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2020-12-18 20:59:06 -0600

    Add files via upload

--- HOME/bin/install_macports.sh
+++ HOME/bin/install_macports.sh
@@ -4,13 +4,13 @@
 #http://truongtx.me/2013/11/08/macports-from-home-directory/
 
 #this is the file to download
-FILE="MacPorts-2.5.4.tar.bz2"
+FILE="MacPorts-2.6.4.tar.bz2"
 
 TMPDIR=/tmp/macports
 
 export PATH=/bin:/sbin:/usr/bin:/usr/sbin
 
-WORKDIR=$HOME/local/macports
+MP_PREFIX=$HOME/local/macports
 
 mkdir -p $WORKDIR
 mkdir -p $TMPDIR
@@ -30,10 +30,19 @@ GROUP=`id -gn`
 
 #./configure --enable-readline --prefix=$WORKDIR --with-install-user=$USER --with-install-group=$GROUP
 #./configure --enable-readline --prefix=$WORKDIR --with-install-user=$USER --with-applications-dir=$HOME/Applications
-./configure --enable-readline --prefix=$WORKDIR --with-install-user=$USER --with-install-group=$GROUP  --with-applications-dir=$HOME/Applications
+
+./configure --enable-readline --prefix=$MP_PREFIX --with-install-user=$USER --with-install-group=$GROUP  --with-applications-dir=$HOME/Applications --without-startupitems
 
 make
 make install
+
+# Needed on ARM with Rosetta: https://trac.macports.org/wiki/BigSurProblems
+echo "build_arch arm64" >> $MP_PREFIX/etc/macports/macports.conf
+# https://guide.macports.org/#internals.configuration-files
+echo "prefix $MP_PREFIX" >> $MP_PREFIX/etc/macports/macports.conf
+echo "applications_dir $HOME/Applications" >> $MP_PREFIX/etc/macports/macports.conf
+echo "startupitem_install no" >> $MP_PREFIX/etc/macports/macports.conf
+
 
 rm -Rf $TMPDIR
 
@@ -42,8 +51,9 @@ cat >> $HOME/.profile << EOF
 
 # MacPorts
 if [ -d \$HOME/local/macports ]; then
-  export PATH=\$HOME/local/macports/bin:\$HOME/local/macports/sbin:\$PATH
-  export MANPATH=\$HOME/local/macports/share/man:\$MANPATH
+  export MP_PREFIX=$HOME/local/macports
+  export PATH=\$MP_PREFIX/bin:\$MP_PREFIX/sbin:\$PATH
+  export MANPATH=\$MP_PREFIX/share/man:\$MANPATH
 fi
 
 EOF
